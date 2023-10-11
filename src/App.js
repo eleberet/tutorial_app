abdc46b7b27dea87abf285f4f1e2f866e480d0e5
el_co <eleberet00>
2023-10-11 11:59:25 +0300

    add post filter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
+import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
 
@@ -11,8 +13,31 @@ function App() {
     { id: 3, title: 'cjavascript 3', body: 'aDescription 3' },
   ])
 
-  const [selectedSort, setSelectedSort] = useState('')
+  //const [selectedSort, setSelectedSort] = useState('') // old
   //const bodyInputRef = useRef() //хук получить доступ к дом элементу и оттуда взять value
+  //const [searchQuery, setSearchQuery] = useState('') // old
+
+  // новый стейт для хранения значения фильтра и отсортированной выборки для отрисовки
+  const [filter, setFilter] = useState({ sort: '', query: '' })
+
+  // пример хука UseMemo - кеширование функции
+  const sortedPosts = useMemo(
+    () => { //коллбек функция
+      console.log('getSortedPosts')
+      if (filter.sort) { // логика сортировки
+        return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+      }
+      return posts
+    },
+    [filter.sort, posts] //массив обьектов при изменении которых отрабатывает коллбек
+  )
+
+  const sortedAndSearchedPosts = useMemo(
+    () => {
+      return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    },
+    [filter.query, sortedPosts]
+  )
 
   const createNewPost = (newPost) => {
     setPosts([newPost, ...posts])
@@ -22,31 +47,23 @@ function App() {
     setPosts(posts.filter((post) => post.id !== postId))
   }
 
-  const sortPosts = (sort) => {
-    console.log('sortPosts', sort)
-    setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort]))) // рвскурить
-  }
+  // сортировка переехала в отдельный компонент PostFilter
+  // const sortPosts = (sort) => { 
+  //   setSelectedSort(sort);
+  // }
 
   return (
     <div className="App">
       <PostForm create={createNewPost} />
       <hr style={{ margin: '15px 0' }} />
-      <div>
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue={"sort"}
-          options={[
-            { value: 'title', name: 'По названию' },
-            { value: 'body', name: 'По описанию' }
-          ]}
-        />
-      </div>
+      
+      <PostFilter 
+        filter={filter}
+        setFilter={setFilter}
+      />
 
-
-      {posts.length ?
-        <PostList deletePost={deletePost} posts={posts} title={'Список постов'} />
+      {sortedAndSearchedPosts.length ?
+        <PostList deletePost={deletePost} posts={sortedAndSearchedPosts} title={'Список постов'} />
         : <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>}
     </div>
   );
